add0b904dd60d4ea335d05ef3ec0828c5ac1f1fb
YasinMakandar <yasinmakandar03@gmail.com>
2024-09-13 00:55:22 +0530

    Added Files

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = 5000;
+const GNEWS_API_KEY = process.env.GNEWS_API_KEY;
+
+app.use(cors());
+app.use(express.json());
+
+// Route to fetch news
+// app.get('/news', async (req, res) => {
+//     const { keyword = '', page = 1, category = '', country = '', language = '' } = req.query;
+//     const articlesPerPage = 10;
+  
+//     try {
+//       const response = await axios.get('https://gnews.io/api/v4/search', {
+//         params: {
+//           q: keyword || 'latest',
+//           token: GNEWS_API_KEY,
+//           lang: language || 'en',
+//           max: articlesPerPage,
+//           page: page,
+//           country: country || '',
+//           category: category || 'general',
+//         },
+//       });
+  
+//       const news = response.data;
+//       res.json({
+//         currentPage: page,
+//         articles: news.articles,
+//         totalArticles: news.totalArticles,
+//         totalPages: Math.ceil(news.totalArticles / articlesPerPage),
+//       });
+//     } catch (error) {
+//       res.status(500).json({ error: 'Error fetching news' });
+//     }
+//   });
+
+
+app.get('/news', async (req, res) => {
+  const { keyword = '', page = 1, category = '', country = '', language = '' } = req.query;
+  const articlesPerPage = 10;
+
+  try {
+    const endpoint = category ? 'https://gnews.io/api/v4/top-headlines' : 'https://gnews.io/api/v4/search';
+    const response = await axios.get(endpoint, {
+      params: {
+        q: keyword || 'latest',
+        token: GNEWS_API_KEY,
+        lang: language || 'en',
+        max: articlesPerPage,
+        page: page,
+        country: country || '',
+        category: category || '',  // Use this parameter if the endpoint supports it
+      },
+    });
+
+    const news = response.data;
+    res.json({
+      currentPage: page,
+      articles: news.articles,
+      totalArticles: news.totalArticles,
+      totalPages: Math.ceil(news.totalArticles / articlesPerPage),
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching news' });
+  }
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
